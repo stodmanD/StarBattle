9bdc2a229d87eeaf304d6fd21b4a65e31a1cbd80
DmitryS <stolypin.dmitry@gmail.com>
2024-05-03 14:30:39 +0300

    Initial commit

---
+++ src/main/java/com/example/starbattle/StarBattleApplication.java
@@ -0,0 +1,13 @@
+package com.example.starbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBattleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarBattleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/starbattle/StarBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBattleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
